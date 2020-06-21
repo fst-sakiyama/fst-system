4ee084d3ad23bd898246e04f1db024bc9db8d5f4
fst-sakiyama <ranpeki@gmail.com>
2020-06-21 17:10:49 +0900

    20200621 h ファイルの分類用のマイグレーションファイルなど一式

---
+++ database/migrations/2020_06_21_165737_create_master_file_classifications_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMasterFileClassificationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('master_file_classifications', function (Blueprint $table) {
+            $table->increments('fileClassificationId');
+            $table->string('fileClassification');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('master_file_classifications');
+    }
+}
